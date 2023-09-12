2cb769a08ba40ebaac5a6c467c12c25c73a26e01
oconsomme <kklingonberry@gmail.com>
2023-09-12 16:23:18 +0900

    23-09-12

---
+++ SpringMVC02/src/main/java/kr/spring/controller/BoardController.java
@@ -0,0 +1,55 @@
+package kr.spring.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import kr.spring.entity.Board;
+import kr.spring.mapper.BoardMapper;
+
+@Controller // 현재 클래스를 핸들러 맵핑이 찾기 위해 컨트롤러로 등록하는 부분
+public class BoardController {
+	
+	@Autowired
+	private BoardMapper mapper; // MyBatis한테 JDBC 실행하게 요청하는 객체
+	
+	@RequestMapping("/") // 요청 url로 들어 왔을 때 아래 기능을 수행하겠다
+	public String home(){
+		System.out.println("홈 기능 수행");
+		return "main";
+	}
+	
+	@RequestMapping("/boardList.do")
+	public @ResponseBody List<Board> boardList() {
+		System.out.println("게시글 전체 보기 기능 수행");
+		List<Board> list = mapper.getLists();
+		System.out.println(list.size());
+		return list;
+	}
+	
+	@RequestMapping("/boardInsert.do")
+	public @ResponseBody void boardInsert(Board board) {
+		System.out.println("게시글 작성 기능 수행");
+		mapper.boardInsert(board);
+	}
+	
+	@RequestMapping("/boardDelete.do")
+	public @ResponseBody void boardDelete(@RequestParam("idx") int idx) {
+		System.out.println("게시글 삭제 기능 수행");
+		mapper.boardDelete(idx);
+	}
+	
+	@RequestMapping("/boardUpdate.do")
+	public @ResponseBody void boardUpdate(Board board) {
+		System.out.println("게시글 삭제 기능 수행");
+		mapper.boardUpdate(board);
+	}
+	
+}
