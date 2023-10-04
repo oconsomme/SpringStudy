040dad2e51eaa3797007d3195d020ae346aeb837
oconsomme <kklingonberry@gmail.com>
2023-10-04 12:39:59 +0900

    23-10-04

---
+++ SpringMVC07/src/main/java/kr/spring/controller/BoardController.java
@@ -0,0 +1,16 @@
+package kr.spring.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class BoardController {
+	
+	@GetMapping("/boardList.do")
+	public String boardList() {
+		
+		return "board/boardList";
+		
+	}
+
+}
---
+++ SpringMVC07/src/test/java/kr/spring/mapper/DataSourceTest.java
@@ -0,0 +1,37 @@
+package kr.spring.mapper;
+
+import java.sql.Connection;
+
+import javax.sql.DataSource;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import lombok.extern.log4j.Log4j;
+
+@Log4j // 테스트 실행결과를 콘솔창에 나오게 하기 위함
+@RunWith(SpringJUnit4ClassRunner.class) // 실행하기위해 스프링 컨테이너에 올리는 코드
+@ContextConfiguration("file:src/main/webapp/WEB-INF/spring/root-context.xml") // root-context.xml 경로를 잡아주는 과정
+public class DataSourceTest {
+	// root-context.xml이 이상없는지 
+	// test하는 클래스
+	
+	// Connection이 잘 되는지 테스트
+	@Autowired // root-context.xml에 있는 id가 dataSource인 것을 사용하겠다
+	private DataSource dataSource;
+	
+	@Test
+	public void testConnection() {
+		
+		try( Connection conn = dataSource.getConnection() ){
+			log.info(conn);
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+		
+	}
+	
+}
